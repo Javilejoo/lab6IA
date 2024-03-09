52601bb781bfb6a18f853397b99650d4fb3b8abd
Javilejoo <pra21486@uvg.edu.gt>
2024-03-09 00:18:19 -0600

    [ADD] game graphics

--- conecta4.py
+++ conecta4.py
@@ -1,4 +1,12 @@
 import numpy as np
+import pygame
+import sys
+import math
+
+Blue = (0,0,255)
+Black = (0,0,0)
+Red = (255,0,0)
+Yellow = (255,255,0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
@@ -8,6 +16,10 @@ def menu():
     print("2. IA vs IA")
     opcion = int(input("Elige una opción: "))
     return opcion
+
+def crear_tablero():
+    tablero = np.zeros((ROW_COUNT,COLUMN_COUNT))
+    return tablero
 
 def drop_piece(tablero, row, col, piece):
         tablero[row][col] = piece
@@ -48,41 +60,93 @@ def winning_move(tablero, piece):
             if tablero[r][c] == piece and tablero[r-1][c+1] == piece and tablero[r-2][c+2] == piece and tablero[r-3][c+3] == piece:
                 return True
 
-def crear_tablero():
-    tablero = np.zeros((ROW_COUNT,COLUMN_COUNT))
-    return tablero
+def draw_board(tablero):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, Blue, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, Black, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+
+
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            if tablero[r][c] == 1:
+                pygame.draw.circle(screen, Red, (int(c*SQUARESIZE+SQUARESIZE/2), height - int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif tablero[r][c] == 2:
+                pygame.draw.circle(screen, Yellow, (int(c*SQUARESIZE+SQUARESIZE/2), height - int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+
+    pygame.display.update()
 
 tablero = crear_tablero()
 print_board(tablero)
 game_over = False
 turno = 0
 
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
+
+size = (width, height)
+RADIUS = int(SQUARESIZE/2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(tablero)
+pygame.display.update()
+ 
+myfont = pygame.font.SysFont("monospace", 75)
+
 while not game_over:
-    # preguntar player 1
-    if turno == 0:
-       col = int(input("Jugador 1, elige una columna (0-6): "))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-       if is_valid_location(tablero, col):
-           row = get_next_open_row(tablero, col)
-           drop_piece(tablero, row, col, 1)
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, Black, (0,0, width, SQUARESIZE))
+            posx = event.pos[0]
+            if turno == 0:
+                pygame.draw.circle(screen, Red, (posx, int(SQUARESIZE/2)), RADIUS)
+            else:
+                pygame.draw.circle(screen, Yellow, (posx, int(SQUARESIZE/2)), RADIUS)
+        pygame.display.update()
 
-           if winning_move(tablero, 1):
-               print("Jugador 1 gana!")
-               game_over = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pygame.draw.rect(screen, Black, (0,0, width, SQUARESIZE))
+            if turno == 0:
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
 
-    # IA hace su movimiento
-    else:
-        col = np.random.randint(0,7)
-        if is_valid_location(tablero, col):
-           row = get_next_open_row(tablero, col)
-           drop_piece(tablero, row, col, 2)
+                if is_valid_location(tablero, col):
+                    row = get_next_open_row(tablero, col)
+                    drop_piece(tablero, row, col, 1)
 
-           if winning_move(tablero, 2):
-               print("Jugador 2 gana!")
-               game_over = True
-               
-    print_board(tablero)
+                    if winning_move(tablero, 1):
+                        label = myfont.render("Jugador 1 gana!!", 1, Red)
+                        screen.blit(label, (40,10))
+                        game_over = True
 
-    turno += 1
-    turno = turno % 2
+            # IA hace su movimiento
+            else:
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+                
+                if is_valid_location(tablero, col):
+                    row = get_next_open_row(tablero, col)
+                    drop_piece(tablero, row, col, 2)
 
+                    if winning_move(tablero, 2):
+                        label = myfont.render("Jugador 2 gana!!", 2, Yellow)
+                        screen.blit(label, (40,10))
+                        game_over = True
+
+            print_board(tablero) 
+            draw_board(tablero)
+
+            turno += 1
+            turno = turno % 2
+
+            if game_over:
+                pygame.time.wait(3000)
+    
